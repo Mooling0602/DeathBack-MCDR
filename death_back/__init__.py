9c49ccd96173ce58a19402fc526f3101615b6590
Mooling0602 <clemooling@outlook.com>
2024-12-10 20:53:40 +0800

    Now can be use as api, update to v0.0.2

--- death_back/__init__.py
+++ death_back/__init__.py
@@ -1,8 +1,10 @@
+import json
+import os
+
 from mcdreforged.api.all import *
 import minecraft_data_api as api
 
 psi = ServerInterface.psi()
-
 position_data = {}
 
 def on_load(server: PluginServerInterface, prev_module):
@@ -32,18 +34,34 @@ def format_position(position: list):
     return formatted_pos
 
 def on_player_death(server, player, event, content):
+    server.logger.info(f"检测到玩家{player}的死亡事件，正在记录其位置...")
     get_player_pos(player)
 
 def tp_back_player(src: CommandSource):
     global position_data
     player = src.player
-    try:
-        player_data = position_data[player]
-        pos = player_data['position']
-        dim = player_data['dimension']
-        loc = format_position(pos)
-        psi.execute(f"execute in {dim} run tp {player} {loc}")
-        del position_data[player]
-        src.reply("传送执行完毕！")
-    except KeyError:
-        src.reply("没有你的死亡点位数据，可能你开服以来还没死过，或者你死后插件才加载或者发生重载。")
+    api_inject = f"{psi.get_data_folder()}/api.json"
+    if os.path.exists(api_inject):
+        try:
+            with open(api_inject, "r", encoding="utf-8") as f:
+                contents = f.read()
+            injected_plg = json.loads(contents)
+        except UnicodeDecodeError:
+            with open(api_inject, "r", encoding="gbk") as f:
+                contents = f.read()
+            injected_plg = json.loads(contents)
+    if not injected_plg["api"]["enabled"] or injected_plg["api"]["register"] is None:
+        try:
+            player_data = position_data[player]
+            pos = player_data['position']
+            dim = player_data['dimension']
+            loc = format_position(pos)
+            psi.execute(f"execute in {dim} run tp {player} {loc}")
+            del position_data[player]
+            src.reply("传送执行完毕！")
+        except KeyError:
+            src.reply("没有你的死亡点位数据，可能你开服以来还没死过，或者你死后插件才加载或者发生重载。")
+    else:
+        if injected_plg["api"]["register"] is not None:
+            plg_name = [injected_plg["api"]["register"]]
+            src.reply(f"插件被用作前置API，无法进行传送，请使用下游插件：{plg_name}的功能！")
